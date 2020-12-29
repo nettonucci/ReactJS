8befecd188ca8a181229a18588c76b3b2152d3f8
Netto Nucci <nettonucci@gmail.com>
2020-12-29 10:22:23 -0300

    Trablhando com useState

---
+++ react-funcional/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function App() {
+  const [numero, setNumero] = useState();
+  const [segundoNumero, setSegundoNumero] = useState();
+  const [resultado, setResultado] = useState();
+
+  const somar = () => {
+    const numeroInt = parseInt(numero);
+    const segnumeroInt = parseInt(segundoNumero);
+
+    setResultado(numeroInt + segnumeroInt);
+  };
+
+  return (
+    <div>
+      Numero 1: <br />
+      <input
+        type="text"
+        value={numero}
+        onChange={e => setNumero(e.target.value)}
+      />{" "}
+      <br />
+      Numero 2: <br />
+      <input
+        type="text"
+        value={segundoNumero}
+        onChange={e => setSegundoNumero(e.target.value)}
+      />
+      <br />
+      <button onClick={somar}>Somar</button> <br />
+      Resultado: <br />
+      <input type="text" value={resultado} /> <br />
+    </div>
+  );
+}
+
+export default App;
